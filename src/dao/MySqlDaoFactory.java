17806074aaebf7c830759b5eb5691bf74d83438d
Github114514 <ti19304003@ga.tera-house.ac.jp>
2021-01-29 10:43:35 +0900

    factory

--- src/dao/MySqlDaoFactory.java
+++ src/dao/MySqlDaoFactory.java
@@ -1,13 +1,25 @@
 package dao;
 
-public class MySqlDaoFactory extends AbstractDaoFactory {
+public class MySqlDaoFactory extends AbstractDaoFactory{
 
-	  public UserDao getUserDao(){
-	    return new MySqlUserDao();
-	  }
+  public UserDao getUserDao(){
+    return new MySqlUserDao();
+  }
+  public CardDao getCardDao(){
+	    return new MySqlCardDao();
+	}
+  public PizzaDao getPizzaDao() {
+	  return new MySqlPizzaDao();
+  }
 
-	  public ConnectionManager getConnectionManager() {
-		  return MySqlConnectionManager.getInstance();
-	  }
+  public PizzaOptionDao getPizzaOptionDao() {
+	  return new MySqlPizzaOptionDao();
+  }
 
+  public SideDao getSideDao() {
+	  return new MySqlSideDao();
+  }
+  public ConnectionManager getConnectionManager() {
+	  return MySqlConnectionManager.getInstance();
+  }
 }
