10e460c0c62f0cac4443970fee85bfb97385c4ac
Github114514 <ti19304003@ga.tera-house.ac.jp>
2021-01-29 11:21:04 +0900

    getttttterrrの追加

--- src/bean/Product.java
+++ src/bean/Product.java
@@ -1,8 +1,11 @@
+// 11時20分 いが
+
 package bean;
 
 public class Product implements Bean{
 	private String name;
 	private String explanation;
+	private String image;
 	private  int price;
 	public Product() {}
 
@@ -32,6 +35,16 @@ public class Product implements Bean{
 
 	public void setPrice(int price) {
 		this.price = price;
+	}
+
+
+	public String getImage() {
+		return image;
+	}
+
+
+	public void setImage(String image) {
+		this.image = image;
 	}
 
 }
