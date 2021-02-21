459a2d0d1392b7716c890f65eac0b7fbcffbccd5
Github114514 <ti19304003@ga.tera-house.ac.jp>
2021-02-21 17:01:08 +0900

    MySqLDeLiVeRyDaO

--- src/dao/MySqlDeliveryDao.java
+++ src/dao/MySqlDeliveryDao.java
@@ -41,15 +41,16 @@ public class MySqlDeliveryDao implements DeliveryDao{
 	    		Addressee Addressee = new Addressee();
 
 	    		Addressee.setOrder_date(rs.getString(1));
-	    		Addressee.setOrderdetail_id(rs.getInt(2));
-	    		Addressee.setUser_number(rs.getInt(3));
-	    		Addressee.setUser_name(rs.getString(4));
-	    		Addressee.setUser_address(rs.getString(5));
-	    		Addressee.setCustom_id(rs.getInt(6));
-	    		Addressee.setProduct_amount(rs.getInt(7));
-	    		Addressee.setOrder_delivery(rs.getBoolean(8));
-	    		Addressee.setProduct_id(rs.getInt(9));
-	    		Addressee.setProduct_name(rs.getString(10));
+	    		Addressee.setOrder_id(rs.getInt(2));
+	    		Addressee.setOrderdetail_id(rs.getInt(3));
+	    		Addressee.setUser_number(rs.getInt(4));
+	    		Addressee.setUser_name(rs.getString(5));
+	    		Addressee.setUser_address(rs.getString(6));
+	    		Addressee.setCustom_id(rs.getInt(7));
+	    		Addressee.setProduct_amount(rs.getInt(8));
+	    		Addressee.setOrder_delivery(rs.getBoolean(9));
+	    		Addressee.setProduct_id(rs.getInt(10));
+	    		Addressee.setProduct_name(rs.getString(11));
 
 	    		list.add(Addressee);
 	    	}
