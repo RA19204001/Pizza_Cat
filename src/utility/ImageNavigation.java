11072b8a689a327a8251add3c06a88d8d75d0c2a
jinngisukann <ht19304025@ga.tera-house.ac.jp>
2021-02-25 11:48:42 +0900

    imageNavigationのあげなおし

--- src/utility/ImageNavigation.java
+++ src/utility/ImageNavigation.java
@@ -9,28 +9,12 @@ import context.RequestContext;
 
 public class ImageNavigation {
 
-		public void imageNavi(String fullPath,String fileName,RequestContext reqc){
+		public void imageNavi(String fileName,RequestContext reqc){
 
 
 
 //	        try {
-//		            //移動元
-//		            FileInputStream input=new FileInputStream(fullPath);
-//		            //移動先
-//		            FileOutputStream output=new FileOutputStream("C:\\PizzaCatImage\\"+fileName+".jpg");
-//
-//		            byte buf[]=new byte[256];
-//		            int len;
-//		            while((len=input.read(buf))!=-1){
-//		            output.write(buf,0,len);
-//	            }
-//		            output.flush();
-//		            output.close();
-//		            input.close();
-//
-//		            System.out.println("ファイル名、場所を変更しました。");
-//	        } catch (Exception e) {
-//        }
+
 			try {
 	            //移動元
 //	            FileInputStream input=new FileInputStream(fullPath);
