e09e0df1c6d1c5ba7be8e54d32aa5fbbcdd95027
Github114514 <ti19304003@ga.tera-house.ac.jp>
2021-02-27 11:54:19 +0900

    ちゅうもんえくせぷしょん

---
+++ src/exception/DisplayOrderHistoryFailedException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class DisplayOrderHistoryFailedException extends IntegrationException {
+	public DisplayOrderHistoryFailedException(String mess, Throwable cause) {
+		super(mess, cause);
+	}
+}
